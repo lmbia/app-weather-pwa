730d3cf30f4b88b222c77743a6ba2583da48f49b
Lucas Biagiotti <64704029+lmbia@users.noreply.github.com>
2022-06-03 10:48:18 -0300

    Update api-keys.js

--- js/api-keys.js
+++ js/api-keys.js
@@ -2,6 +2,6 @@ export { WEATHER_API_KEY, MAPS_API_KEY };
 
 /* Insertar las APIs-KEYS correspondientes para utilizar la aplicación */
 
-const WEATHER_API_KEY = ' [ API_KEY WEATHER ] ';
-const MAPS_API_KEY = ' [ API_KEY GOOGLE_MAPS ] '; 
+const WEATHER_API_KEY = '48599f7b1dbfd1805d0bd853d791da40';
+const MAPS_API_KEY = 'AIzaSyBObHbJM9NjSAPd-nnv_4owBcue6KlBBNo'; 
 
